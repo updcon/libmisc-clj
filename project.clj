76a6422a9b0b69e558a134978fdc2962d5f646d9
MelKori <mieldo@gmail.com>
2021-08-19 22:51:27 +0300

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.dkdhub/libmisc-clj "0.2.1"
+(defproject com.dkdhub/libmisc-clj "0.2.2"
   :description "DKD Core Engine lib of miscellaneous"
   :url "http://dkdhub.com"
   :license {:name "Proprietary"
