492d7ec0cf0faeb98db1ed52446e12fa1f9a29c1
MelKori <mieldo@gmail.com>
2022-05-25 21:38:27 +0300

    update deps

--- profiles.clj
+++ profiles.clj
@@ -1,12 +1,12 @@
-{:dev      {:plugins             [[org.apache.maven.wagon/wagon-ssh-external "3.4.3"]
-                                  [org.apache.maven.wagon/wagon-http-lightweight "3.4.3"]]
+{:dev      {:plugins             [[org.apache.maven.wagon/wagon-ssh-external "3.5.1"]
+                                  [org.apache.maven.wagon/wagon-http-lightweight "3.5.1"]]
 
             :plugin-repositories [["private-jars" "http://local.repo:9180/repo"]]
             :deploy-repositories [["private-jars-scp" {:url              "scp://local.repo/home/clojar/data/dev_repo/"
                                                        :username         "clojar"
                                                        :private-key-file :env/clojure_ssh_key}]]}
- :provided {:dependencies      [[org.clojure/clojure "1.10.3"]
-                                [org.clojure/tools.namespace "1.1.0"]]
+ :provided {:dependencies      [[org.clojure/clojure "1.11.1"]
+                                [org.clojure/tools.namespace "1.3.0"]]
             :global-vars       {*warn-on-reflection* true}
             :jar-exclusions    [#"\.java"]
 
--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject com.dkdhub/libmisc-clj "0.2.4"
+(defproject com.dkdhub/libmisc-clj "0.2.5"
   :description "DKD Core Engine lib of miscellaneous"
   :url "http://dkdhub.com"
   :license {:name "Proprietary"
             :url  "https://dkdhub.com/licenses/base.html"}
 
-  :dependencies [[cheshire "5.10.1"]
+  :dependencies [[cheshire "5.10.2"]
                  [commons-io "2.11.0"]
                  [clj-time "0.15.2"]
                  [com.eaio.uuid/uuid "3.2"]
-                 [org.postgresql/postgresql "42.2.23"]]
+                 [org.postgresql/postgresql "42.3.6"]]
 
   :omit-source false
   :aot [libmisc-clj.jnio-proto])
