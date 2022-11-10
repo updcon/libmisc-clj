54ce94e70d54e62bc34a416456e90897f4029247
MelKori <mieldo@gmail.com>
2022-11-10 14:56:06 +0200

    make constants more optimal in runtime

--- src-clj/libmisc_clj/vars.clj
+++ src-clj/libmisc_clj/vars.clj
@@ -1,6 +1,11 @@
 (ns libmisc-clj.vars)
 
-(defonce ^:static ttl_1m (* 1000 60))                                    ;seconds @ minute
-(defonce ^:static ttl_1h (* ttl_1m 60))                                  ;mins @ hour
-(defonce ^:static ttl_1d (* ttl_1h 24))                                  ;day
-(defonce ^:static ttl_1w (* ttl_1d 7))                                   ;week
+(defonce ^:const ^:static ttl_1m (* 1000 60))                                    ;seconds @ minute
+(defonce ^:const ^:static ttl_1h (* ttl_1m 60))                                  ;mins @ hour
+(defonce ^:const ^:static ttl_1d (* ttl_1h 24))                                  ;day
+(defonce ^:const ^:static ttl_1w (* ttl_1d 7))                                   ;week
+
+(def ^:const ^:static time-units-ms
+  (apply hash-map
+         (interleave [:millisecond :second :minute :hour :day :week]
+                     (reductions * [1 1000 60 60 24 7]))))
