c25b386c9d6b349d172c6afc4f791ed5fa1c20d1
MelKori <mieldo@gmail.com>
2022-12-09 11:38:29 +0200

    added separate profiles for j8 and j11

--- profiles.clj
+++ profiles.clj
@@ -5,6 +5,7 @@
             :deploy-repositories [["private-jars-scp" {:url              "scp://local.repo/home/clojar/data/dev_repo/"
                                                        :username         "clojar"
                                                        :private-key-file :env/clojure_ssh_key}]]}
+
  :provided {:dependencies      [[org.clojure/clojure "1.11.1"]
                                 [org.clojure/tools.namespace "1.3.0"]]
             :global-vars       {*warn-on-reflection* true}
@@ -14,9 +15,13 @@
             :resource-paths    ["resources"]
 
             :java-source-paths #{"src-java"}
-            :javac-options     ["-source" "8" "-target" "1.8" "-g:none"]
 
             :target-path       "target"
             :clean-targets     ^{:protect false} [:target-path :compile-path]}
+
+ :j8       {:javac-options ["-source" "8" "-target" "8" "-g:none"]}
+
+ :j11      {:javac-options ["-source" "11" "-target" "11" "-g:none"]}
+
  :jar      {:aot :all}}
 
